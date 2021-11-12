8f14caf9eed34abff1f47c45549005e2658db7ce
Giulia Agostinetto <36715478+giuliaago@users.noreply.github.com>
2021-11-13 00:23:45 +0100

    Add files via upload

---
+++ microFIM_example_code.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import csv
+from csv import writer
+import readline
+import re
+import string
+
+import fim
+import functions.microdir as md
+import functions.microfim as mf
+import functions.microimport as mi
+import functions.microinterestmeasures as mim
+
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from sklearn.metrics import pairwise_distances
+from sklearn import manifold
